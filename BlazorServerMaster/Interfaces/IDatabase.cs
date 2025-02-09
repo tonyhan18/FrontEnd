18dd29dab344d169c96adb0f201d85ca242e81a9
tonyhan18 <ourclub7279@gmail.com>
2025-02-09 11:57:55 +0900

    250209 updated - blazor 프로그램 업데이트

---
+++ BlazorServerMaster/Interfaces/IDatabase.cs
@@ -0,0 +1,17 @@
+﻿using BlazroServerMaster.Models;
+
+namespace BlazroServerMaster.Interfaces
+{
+    public interface IDatabase<T>
+    {
+        Task<List<T>> GetAsync();
+
+        T GetDetail(int? id);
+
+        void Create(T entity);
+
+        void Update(int? id, T entity);
+
+        void Delete(int? id);
+    }
+}
---
+++ BlazorServerMaster/Services/GangnamguPopulationService.cs
@@ -0,0 +1,89 @@
+﻿using BlazroServerMaster.Interfaces;
+using BlazroServerMaster.Models;
+using Humanizer.DateTimeHumanizeStrategy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazroServerMaster.Services
+{
+    public class GangnamguPopulationService : IDatabase<GangnamguPopulation>
+    {
+        private readonly BlazorServerDbContext? _context;
+
+        public GangnamguPopulationService(BlazorServerDbContext context)
+        {
+            this._context = context;
+        }
+
+        public void Create(GangnamguPopulation entity)
+        {
+            if (_context?.GangnamguPopulations == null) 
+            {
+                throw new NullReferenceException();
+            }
+
+            this._context.GangnamguPopulations.Add(entity);
+            this._context?.SaveChanges();        
+        }
+
+        public void Delete(int? id)
+        {
+            if (_context?.GangnamguPopulations == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            var validData = this._context.GangnamguPopulations.Find(id);
+
+            if (validData != null)
+            {
+                this._context.GangnamguPopulations.Remove(validData);
+                this._context.SaveChanges();
+            }
+        }
+
+        public async Task<List<GangnamguPopulation>> GetAsync()
+        {
+            return await this._context.GangnamguPopulations.ToListAsync();
+        }
+
+        public GangnamguPopulation GetDetail(int? id)
+        {
+            if (id == null || _context?.GangnamguPopulations == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            var validData = this._context.GangnamguPopulations.FirstOrDefault(x => x.Id == id);
+
+            if (validData != null)
+            {
+                return validData;
+            }
+            else 
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public void Update(int? id, GangnamguPopulation entity)
+        {
+            if (id == null || _context?.GangnamguPopulations == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            var validData = this._context.GangnamguPopulations.FirstOrDefault(x => x.Id == id);
+            
+            if (validData != null)
+            {
+                this._context.GangnamguPopulations.Update(entity);
+                this._context.SaveChanges();
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
